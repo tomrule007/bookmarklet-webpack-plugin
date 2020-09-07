8a2bead4094120b603f64afd52e81d8e028f6200
Thomas Herzog <Tomrule007@gmail.com>
2020-09-07 14:15:11 -0700

    Add dependencies and configs/ignore files

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: { 'prettier/prettier': ['error'] },
+};
